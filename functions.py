0d76bceffbd966aaace41e6a6fcee79c7f85ced2
Maks <maks.mashtaler99@gmail.com>
2022-03-28 12:31:53 +0300

    initial commit

--- functions.py
+++ functions.py
@@ -1,1 +1,19 @@
-import json
+import json, re
+
+posts = []
+POST_PATH = 'posts.json'
+
+
+def load_posts_from_json():
+    global posts
+    with open(POST_PATH, encoding='UTF-8') as fp:
+        posts = json.load(fp)
+        return posts
+
+
+def upload_posts(posts):
+    with open(POST_PATH, 'w', encoding='UTF-8') as file:
+        json.dump(posts, file, indent=4)
+
+
+
